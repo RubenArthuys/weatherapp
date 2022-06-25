08021e3df3362c7618ce54fff8a50525296d78b3
RubenArthuys <ruben.arthuys@gmail.com>
2022-06-25 15:11:35 +0200

    part4

--- part4/models/connection.js
+++ part4/models/connection.js
@@ -6,10 +6,9 @@ var options = {
   useNewUrlParser: true,
   useUnifiedTopology : true
  }
- mongoose.connect('mongodb+srv://rubenarth:ilypo002@cluster0.v5blhg2.mongodb.net/weatherapp?retryWrites=true&w=majority',
+ mongoose.connect('mongodb+srv://rubenarth:XH2uztbN9ttgnTVQ@cluster0.v5blhg2.mongodb.net/weatherapp?retryWrites=true&w=majority',
     options,        
     function(err) {
      console.log(err);
     }
  )
-
